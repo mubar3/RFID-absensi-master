dc098cc61d2ef0b22d7ef1df424b186c1e1591a8
mubar3 <55468410+mubar3@users.noreply.github.com>
2022-08-26 10:34:15 +0700

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -29,7 +29,7 @@
 
     <!-- Custom scripts for all pages-->
     <script src="./asset/js/sb-admin-2.js"></script>
-    <script src="./asset/js/sb-admin-2.min.js"></script>
+    <!-- <script src="./asset/js/sb-admin-2.min.js"></script> -->
 
 
     <!-- Page level plugins -->
